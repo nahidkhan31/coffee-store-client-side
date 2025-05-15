c18ecd65922fc83b04aa9c5a419eeccce81ff875
Nahid Khan <nahidkhanniloy31@gmail.com>
2025-05-15 18:18:38 +0600

    added form

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,9 +1,91 @@
 import React from "react";
 
 const AddCoffee = () => {
+  const handleAddCoffee = (e) => {
+    e.preventDefault();
+    const form = e.target;
+  };
+
   return (
-    <div>
-      <h1>coffee</h1>
+    <div className="p-24">
+      <div className="p-12 text-center space-y-4">
+        <h1 className="text-6xl">Add New Coffee</h1>
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+          quod odio totam officiis optio omnis! Rem dolorum fugit modi saepe.
+        </p>
+      </div>
+      <form onSubmit={handleAddCoffee}>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Name</label>
+            <input
+              type="text"
+              name="name"
+              className="input w-full"
+              placeholder="Coffee Name"
+            />
+          </fieldset>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Chef</label>
+            <input
+              type="text"
+              name="chef"
+              className="input w-full"
+              placeholder="Chef Name"
+            />
+          </fieldset>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Supplier</label>
+            <input
+              type="text"
+              name="supplier"
+              className="input w-full"
+              placeholder="Supplier Name"
+            />
+          </fieldset>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Taste</label>
+            <input
+              type="text"
+              name="taste"
+              className="input w-full"
+              placeholder="Taste Name"
+            />
+          </fieldset>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Category</label>
+            <input
+              type="text"
+              name="category"
+              className="input w-full"
+              placeholder="Category Name"
+            />
+          </fieldset>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+            <label className="label">Details</label>
+            <input
+              type="text"
+              name="details"
+              className="input w-full"
+              placeholder="Details Name"
+            />
+          </fieldset>
+        </div>
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border my-6 p-4">
+          <label className="label">Photo</label>
+          <input
+            type="text"
+            name="Photo"
+            className="input w-full"
+            placeholder="Photo URL"
+          />
+        </fieldset>
+
+        <input type="submit" className="btn w-full" value="Add Coffee" />
+      </form>
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1>header</h1>
+      <h1 className="text-2xl">header</h1>
     </div>
   );
 };
